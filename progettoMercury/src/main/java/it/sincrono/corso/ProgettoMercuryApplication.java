b0b11df2b82f4c1590d0b74d9a60bfb7db8d465f
Sincrono <Sincrono@DESKTOP-Q20RCTL>
2019-05-29 14:40:45 +0200

    1

---
+++ progettoMercury/src/main/java/it/sincrono/corso/ProgettoMercuryApplication.java
@@ -0,0 +1,13 @@
+package it.sincrono.corso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoMercuryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoMercuryApplication.class, args);
+	}
+
+}
---
+++ progettoMercury/src/main/java/it/sincrono/corso/controller/MainController.java
@@ -0,0 +1,11 @@
+package it.sincrono.corso.controller;
+
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class MainController {
+
+	
+
+}
