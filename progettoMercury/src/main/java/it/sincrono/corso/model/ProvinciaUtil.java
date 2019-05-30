182956c6a794b955e556790eeed116ca7fa2e9e1
Utente <Utente@DESKTOP-VLPD86O>
2019-05-30 16:33:36 +0200

    Luca 7

---
+++ progettoMercury/src/main/java/it/sincrono/corso/model/ProvinciaUtil.java
@@ -0,0 +1,7 @@
+package it.sincrono.corso.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProvinciaUtil extends JpaRepository<Provincia, Integer> {
+
+}
---
+++ progettoMercury/src/main/java/it/sincrono/corso/model/RegioneUtil.java
@@ -0,0 +1,8 @@
+package it.sincrono.corso.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegioneUtil extends JpaRepository<Regione, Integer>
+{
+
+}
