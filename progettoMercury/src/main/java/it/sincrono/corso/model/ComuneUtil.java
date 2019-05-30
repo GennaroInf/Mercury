f6b0c339dcffc6669b9f342edf03b718e79d8928
Utente <Utente@DESKTOP-VLPD86O>
2019-05-30 14:18:06 +0200

    quarto

--- progettoMercury/src/main/java/it/sincrono/corso/model/ComuneUtil.java
+++ progettoMercury/src/main/java/it/sincrono/corso/model/ComuneUtil.java
@@ -1,5 +1,13 @@
 package it.sincrono.corso.model;
 
-public interface ComuneUtil {
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ComuneUtil extends CrudRepository<Comune, Integer> {
+
+List<Comune> findById();
+
+
 
 }
